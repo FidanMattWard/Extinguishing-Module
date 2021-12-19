a73f7fae3ef05a882eb64f059645e05a49d1a7b8
Fidan Matteus Ward <71519692+FidanMattWard@users.noreply.github.com>
2021-12-20 00:48:36 +0300

    Update Set Detector Params.js

--- Smoke Detector Control/Set Detector Params.js
+++ Smoke Detector Control/Set Detector Params.js
@@ -7,7 +7,7 @@ let newMsgType = {};
 newMsg = {
     "method": "setValveState",
     "concentration": msg.concentration,
-    "alarm": getAlarmStatus(msg.concentration)
+    "alarm": getAlarmStatus(msg.concentration) ? 1 : 0
 };
 newMsgType = "POST_ATTRIBUTES_REQUEST";
 return {msg: newMsg, metadata: metadata, msgType: newMsgType};
