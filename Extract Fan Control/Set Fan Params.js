bc2d9415e997e1e1b834ea6856bd833330457d42
Fidan Matteus Ward <71519692+FidanMattWard@users.noreply.github.com>
2021-12-20 00:48:24 +0300

    Update Set Fan Params.js

--- Extract Fan Control/Set Fan Params.js
+++ Extract Fan Control/Set Fan Params.js
@@ -7,7 +7,7 @@ let newMsgType = {};
 newMsg = {
     "method": "setValveState",
     "concentration": msg.concentration,
-    "status": getVentStatus(msg.concentration)
+    "status": getVentStatus(msg.concentration) ? 1 : 0
 };
 newMsgType = "POST_ATTRIBUTES_REQUEST";
 return {msg: newMsg, metadata: metadata, msgType: newMsgType};
