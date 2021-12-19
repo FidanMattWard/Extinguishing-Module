9fc0c85db50577ea7d2ffc9cd16f7f66deb29114
Fidan Matteus Ward <71519692+FidanMattWard@users.noreply.github.com>
2021-12-20 00:48:10 +0300

    Update Set Extinguisher Params.js

--- Extinguishing Control/Set Extinguisher Params.js
+++ Extinguishing Control/Set Extinguisher Params.js
@@ -7,7 +7,7 @@ let newMsgType = {};
 newMsg = {
     "method": "setValveState",
     "concentration": msg.concentration,
-    "inergen": getSupplyInergenStatus(msg.concentration, msg.flag)
+    "inergen": getSupplyInergenStatus(msg.concentration, msg.flag) ? 1 : 0
 };
 newMsgType = "POST_ATTRIBUTES_REQUEST";
 return {msg: newMsg, metadata: metadata, msgType: newMsgType};
